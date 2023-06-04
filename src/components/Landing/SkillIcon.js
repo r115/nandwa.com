012ea15b2ceafd0ee1114f93eae03d2822994f1f
Bernard Nandwa <r115codes@gmail.com>
2023-06-04 13:06:34 +0300

    switch to t3

--- src/components/Landing/SkillIcon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function SkillIcon({iconURL,href}) {
-  return(
-    <a className={'h-24 w-24 overflow-hidden block'} href={href}>
-      <img src={iconURL} alt="" className={'block'}/>
-    </a>
-  )
-}

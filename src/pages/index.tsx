c89d95723e479ca1053c08a78e9dc30b925a923f
Bernard Nandwa <r115codes@gmail.com>
2023-06-04 13:09:41 +0300

    retain old bg colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <title>Bernard Nandwa</title>
         <meta name="description" content="ramblings from an internet newbie" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-[#262325]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Bernard Nandwa

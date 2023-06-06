f15a65af94f4305b74515f4a66a7672669bd9bf6
Bernard Nandwa <bernard@nandwa.com>
2023-06-06 22:28:08 +0300

    refactor: good ol next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { api } from "@nandwa/utils/api";
 import "@nandwa/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const MyApp: AppType = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Component {...pageProps} />
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;

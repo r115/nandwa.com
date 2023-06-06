cc2333aad60e612e2d30d48bb38c1ccd497c78bf
Bernard Nandwa <bernard@nandwa.com>
2023-06-06 23:12:17 +0300

    feat: init harambee stars page

---
+++ src/pages/projects/sports/harambee-stars/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next"
+
+const HarambeeStars: NextPage = () => {
+    return (
+        <div className="bg-gray-900 min-h-screen">
+            <nav className="">
+                <h1 className="text-white">Harambee Stars</h1>
+            </nav>
+            <main>
+                <div className="container">
+                    Next Match
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default HarambeeStars;
